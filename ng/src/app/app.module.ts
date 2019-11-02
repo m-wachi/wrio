846c5b0db1b1e5a708e45f5a07daeb130906dda2
m-wachi <m-wachi-ipad@deb02.us-central1-a.c.mesmerizing-app-252403.internal>
2019-11-02 15:33:44 +0000

    edit for new angular version

--- ng/src/app/app.module.ts
+++ ng/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MaintblComponent } from './maintbl/maintbl.component';
 import { MessagesComponent } from './messages/messages.component';
@@ -14,6 +15,7 @@ import { MessagesComponent } from './messages/messages.component';
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     HttpClientModule
   ],
   providers: [],
