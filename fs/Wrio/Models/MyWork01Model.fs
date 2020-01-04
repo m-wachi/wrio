99cedffdff39f4b9fef2949bbcc70eb1609ac506
m-wachi <m-wachi-ipad@deb02.us-central1-a.c.mesmerizing-app-252403.internal>
2020-01-04 03:46:01 +0000

    add F# ASP.NET server code.

---
+++ fs/Wrio/Models/MyWork01Model.fs
@@ -0,0 +1,6 @@
+namespace Aspnet2.Models
+
+type MyWork01Model(id: int, name: string) =
+    member this.Id = id
+    member this.Name = name
+
---
+++ fs/Wrio/WeatherForecast.fs
@@ -0,0 +1,11 @@
+namespace Aspnet2
+
+open System
+
+type WeatherForecast =
+    { Date: DateTime
+      TemperatureC: int
+      Summary: string }
+
+    member this.TemperatureF =
+        32 + (int (float this.TemperatureC / 0.5556))
