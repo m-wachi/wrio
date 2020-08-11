d7819399dbc2c81dae48d4456de01a8fc3d6b58a
m-wachi <m-wachi@example.com>
2020-08-11 12:38:12 +0900

    add code.

--- tswork/src/index.ts
+++ tswork/src/index.ts
@@ -24,19 +24,28 @@ console.log("Hello World.");
 
 // {"sales_date":"2019-07-01T00:00:00","item_name":"アイテム０１","nof_sales":10}
 
+/*
 var rec1 = {"sales_date":"2019-07-01T00:00:00","item_name":"アイテム０１","nof_sales":10};
 var rec2 = {"sales_date":"2019-07-01T00:00:00","item_name":"アイテム０２","nof_sales":15};
 var rec3 = {"sales_date":"2019-07-02T00:00:00","item_name":"アイテム０１","nof_sales":20};
 var rec4 = {"sales_date":"2019-07-02T00:00:00","item_name":"アイテム０２","nof_sales":25};
+*/
 
-console.log(rec1);
+var recs = [];
+recs.push({"sales_date":"2019-07-01T00:00:00","item_name":"アイテム０１","nof_sales":10});
+recs.push({"sales_date":"2019-07-01T00:00:00","item_name":"アイテム０２","nof_sales":15});
+recs.push({"sales_date":"2019-07-02T00:00:00","item_name":"アイテム０１","nof_sales":20});
+recs.push({"sales_date":"2019-07-02T00:00:00","item_name":"アイテム０２","nof_sales":25});
+
+
+//console.log(rec1);
 
 var colNames = ["sales_date", "item_name"];
 
 console.log(colNames);
 
 var d1 = {};
-
+/*
 var d2 = func01(colNames, 0, 1, d1, rec1);
 
 console.log("==== d2 ====");
@@ -51,6 +60,14 @@ console.log(d1);
 
 var d2 = func01(colNames, 0, 1, d1, rec3);
 var d2 = func01(colNames, 0, 1, d1, rec4);
+
+console.log("==== d1 ====");
+console.log(d1);
+*/
+
+recs.forEach(rec => {
+  func01(colNames, 0, 1, d1, rec);
+});
 
 console.log("==== d1 ====");
 console.log(d1);
