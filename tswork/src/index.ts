9ab35846ba109c39a6446a40b602ed9dab419372
m-wachi <m-wachi@example.com>
2020-08-17 13:04:20 +0900

    add code

--- tswork/src/index.ts
+++ tswork/src/index.ts
@@ -1,4 +1,4 @@
-﻿import moment from 'moment';
+﻿import moment, { isMoment } from 'moment';
 
 interface WrioDict<T> {
   [key: string] : T;
@@ -94,6 +94,9 @@ recs.push({"sales_date":moment("2019-07-02T00:00:00"),"item_name":"アイテム�
 recs.push({"sales_date":moment("2019-07-02T00:00:00"),"item_name":"アイテム０２","nof_sales":25});
 recs.push({"sales_date":moment("2019-07-01T00:00:00"),"item_name":"アイテム０１","nof_sales":3});
 
+console.log("ismoment");
+console.log(isMoment(recs[0]["sales_date"]));
+
 
 var pvtColHdrs = ["item_name"];
 var pvtRowHdrs = ["sales_date"];
@@ -102,8 +105,8 @@ var colNames = pvtColHdrs.concat(pvtRowHdrs);
 
 console.log(colNames);
 
-var d1 = {};
 /*
+var d1 = {};
 var d2 = func01(colNames, 0, 1, d1, rec1);
 
 console.log("==== d2 ====");
@@ -121,16 +124,16 @@ var d2 = func01(colNames, 0, 1, d1, rec4);
 
 console.log("==== d1 ====");
 console.log(d1);
-*/
 
 recs.forEach(rec => {
   func01(colNames, 0, d1, rec);
 });
+*/
 
-console.log("==== d1 ====");
-console.log(d1);
+var pvt1 = func01s(colNames, recs);
 
-console.log(Object.keys(d1));
+console.log("==== pvt1 ====");
+console.log(pvt1);
 
 let k1s = recs.map(rec => {
   var a : WrioDict<any> = {};
@@ -153,7 +156,7 @@ console.log(k2Sets);
 let k3Sets = func02(colNames, recs);
 console.log("===== k3Sets ====");
 console.log(k3Sets);
-
+/*
 let o1 = {"sales_date":"2019-07-01T00:00:00"};
 let o2 = {"sales_date":"2019-07-01T00:00:00"};
 let o3 = {"sales_date":"2019-07-02T00:00:00"};
@@ -167,4 +170,57 @@ console.log("o3 = " + String(o3["sales_date"]));
 let day1 = moment("2019-07-01T00:00:00");
 console.log("day1 = [" + String(day1) + "]");
 let o1a = {"sales_date": day1};
+*/
+
+var pvtColHdr2 = func02(pvtColHdrs, recs);
+console.log("===== pvtColHdr2 ====");
+console.log(pvtColHdr2);
+
+var pvtRowHdr2 = func02(pvtRowHdrs, recs);
+console.log("===== pvtRowHdr2 ====");
+console.log(pvtRowHdr2);
+
+console.log("-----------------------");
+var s1 = "| |" + Object.keys(pvtColHdr2).join("|") + "|";
+
+console.log(s1);
+
+let pvtColHdr3 : any[] = Object.keys(pvtColHdr2);
+
+/*
+var colKey1 = Object.keys(pvtColHdr2)[0];
+var colKey2 = Object.keys(pvtColHdr2)[1];
+*/
+
+var pvtRowHdr3 : any[] = Object.keys(pvtRowHdr2);
+var rowKey1 = Object.keys(pvtRowHdr2)[0];
+
+console.log("ismoment");
+console.log(isMoment(pvtRowHdr3[0]));
+
+s1 = "|"  + pvtRowHdr3[0] + "|";
+
+
+let valProps = ["nof_sales"];
+/*
+var o1 = pvt1[colKey1][rowKey1];
+s1 += o1[valProps[0]] + "|";
+var o2 = pvt1[colKey2][rowKey1];
+s1 += o2[valProps[0]] + "|";
+*/
+for (const colHdr of pvtColHdr3) {
+  let o1 = pvt1[colHdr][pvtRowHdr3[0]];
+  s1 += o1[valProps[0]] + "|";
+}
+console.log(s1);
+
+s1 = "|"  + pvtRowHdr3[1] + "|";
+for (const colHdr of pvtColHdr3) {
+  let o1 = pvt1[colHdr][pvtRowHdr3[1]];
+  s1 += o1[valProps[0]] + "|";
+}
+
+
+console.log(s1);
+console.log("-----------------------");
 
