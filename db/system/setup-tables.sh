a74de008462b3a263ed1f8d3c327bf01ac52ce34
m-wachi <dev01@deb08.us-central1-a.c.silver-optics-288714.internal>
2020-10-06 13:35:37 +0900

    add code

--- db/system/setup-tables.sh
+++ db/system/setup-tables.sh
@@ -4,7 +4,3 @@ psql -f m_dataset.sql
 psql -f m_ds_table.sql
 psql -f m_pivot.sql
 psql -f m_ds_join.sql 
-psql -f ins_m_dataset.sql
-psql -f ins_m_ds_table.sql
-psql -f ins_m_pivot.sql
-psql -f ins_m_ds_join.sql 
