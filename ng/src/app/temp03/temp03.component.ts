456bfba34c4e5fa99a17d43ae34146fe70c089a0
m-wachi <dev01@deb02.us-central1-a.c.strategic-lens-297204.internal>
2021-01-21 12:51:44 +0900

    add code.

--- ng/src/app/temp03/temp03.component.ts
+++ ng/src/app/temp03/temp03.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PivotData, OptPivotData } from '../pivotdata';
-import { Pivot, DsColumn } from '../model';
+import { Pivot, DtSet, DsColumn } from '../model';
 import { PivotService } from '../pivot.service';
 import { MessageService } from '../message.service';
 
@@ -12,11 +12,13 @@ import { MessageService } from '../message.service';
 export class Temp03Component implements OnInit {
 
   pivot: Pivot = null;
+  dataSet: DtSet = null;
   sPivot: string = "";
   optPivotData: OptPivotData = null;
   pivotData: PivotData = null;
   factColumns: Array<DsColumn> = [];
   aryColumn: Array<DsColumn> = [];
+
 
   constructor(private pivotSvc: PivotService, 
               private messageSvc: MessageService) { }
@@ -25,6 +27,7 @@ export class Temp03Component implements OnInit {
     this.pivotSvc.getPivot(pivotId).subscribe(
       optPvt => {
         this.pivot = optPvt.value;
+        this.dataSet = this.pivot.dataSet;
         let colHdr = this.pivot.setting.colHdr;
         let sPivot = JSON.stringify(this.pivot);
         this.sPivot = sPivot;
@@ -37,8 +40,8 @@ export class Temp03Component implements OnInit {
   }
 
   button1_Click(): void {
-    this.sPivot = sessionStorage.getItem("pivot");
-    this.pivot = JSON.parse(this.sPivot);
+    //this.sPivot = sessionStorage.getItem("pivot");
+    //this.pivot = JSON.parse(this.sPivot);
     this.pivotSvc.putPivotData01(this.pivot).subscribe(
       optPvtDt => {
         this.optPivotData = optPvtDt;
@@ -72,18 +75,15 @@ export class Temp03Component implements OnInit {
 
 
   /***** ドラッグ開始時の処理 *****/
-  f_dragstart(event){
+  column_Dragstart(event){
     event.dataTransfer.effectAllowed = "copy";
-    //this.messageSvc.add("drag start.");
-    console.log("drag start.");
     //ドラッグするデータのid名をDataTransferオブジェクトにセット
     event.dataTransfer.setData("text", event.target.id);
   }
   
-  /***** ドラッグ要素がドロップ要素に重なっている間の処理 *****/
-  f_dragover(event){
-    //dragoverイベントをキャンセルして、ドロップ先の要素がドロップを受け付けるようにする
-    //console.log("dragover start.");
+  //drag-over event handler for column/row header element
+  hdr_Dragover(event){
+    //cancel dragover event and enable drop event 
     event.preventDefault();
     event.dataTransfer.dropEffect = "copy";
   }
@@ -91,7 +91,7 @@ export class Temp03Component implements OnInit {
 
   /***** ドロップ時の処理 *****/
   f_drop(event){
-    event.preventDefault();
+    //event.preventDefault();
     event.dataTransfer.dropEffect = "copy";
     console.log("drop start.");
     //ドラッグされたデータのid名をDataTransferオブジェクトから取得
@@ -103,18 +103,83 @@ export class Temp03Component implements OnInit {
     console.log("drag_elm=" + drag_elm);
     console.log("drag_elem_txt=" + drag_elem_txt);
 
+
     //let dropElm = drag_elm.cloneNode(true) as HTMLElement;
-    let dropElm = document.createElement("span");
-    dropElm.id = "rowHdr_" + id_name;
-    dropElm.textContent = drag_elem_txt;
+    //let dropElm = document.createElement("span");
+    //dropElm.id = "rowHdr_" + id_name;
+    //dropElm.textContent = drag_elem_txt;
 
     //ドロップ先にドラッグされた要素を追加
     //event.currentTarget.appendChild(drag_elm); //これだと移動になる模様
-    event.currentTarget.appendChild(dropElm); //これだと移動になる模様
+    //event.currentTarget.appendChild(dropElm); //これだと移動になる模様
     //エラー回避のため、ドロップ処理の最後にdropイベントをキャンセルしておく
     
-    //this.pivot.setting.colHdr.push(drag_elem_txt);  // これはなにも影響しない
+    this.pivot.setting.colHdr.push(drag_elem_txt);  // event.preventDefault() するとなにも画面に影響しない
     
   }
+
+  // column header drop event
+  colHdr_Drop(event) {
+    this.hdrDrop(event, 1);
+  }
+  // row header drop event
+  rowHdr_Drop(event) {
+    this.hdrDrop(event, 2);
+  }
+
+  // column and row header drop event common routine.
+  hdrDrop(event, colrow: number) {
+    //event.preventDefault(); DO NOT preventDefault(). component not redrawing. 
+    event.dataTransfer.dropEffect = "copy";
+    //get id attribute from "DataTransfer" object.
+    let elmId = event.dataTransfer.getData("text");
+    console.log("drop element id=" + elmId);
+    //get element from id
+    var elmDrag =document.getElementById(elmId);
+    let sColumn = elmDrag.textContent;
+
+    if (colrow === 1) {
+      this.pivot.setting.colHdr.push(sColumn);
+    } else if (colrow === 2) {
+      this.pivot.setting.rowHdr.push(sColumn);
+    }
+
+  }
+
+  //fact or dimention table header click handler
+  //toggle column name visibility.
+  tblHdr_Click(tblName : string) {
+    console.log("tblHdr_Click: " + tblName);
+
+    let tblFact = document.getElementById("tbl_" + tblName) as HTMLTableElement;
+    let tBodiesLength = tblFact.tBodies.length;
+    for (let i=0; i<tBodiesLength; i++) {
+      tblFact.tBodies.item(i).hidden = !tblFact.tBodies.item(i).hidden; 
+    }
+    
+  }
+
+  //remove header columns
+  colHdrRemove_Click(colIndex: number) {
+    console.log("call colHdrRemove_Click(" + colIndex.toString() + ")");
+    this.removeColumn(colIndex, 1);
+  }
+
+  //remove header columns
+  rowHdrRemove_Click(colIndex: number) {
+    console.log("call rowHdrRemove_Click(" + colIndex.toString() + ")");
+    this.removeColumn(colIndex, 2);
+  }
+
+  removeColumn(colIndex: number, colrow: number) {
+
+    if (colrow === 1) {
+      this.pivot.setting.colHdr.splice(colIndex ,1);
+    } 
+    else if (colrow === 2) {
+      this.pivot.setting.rowHdr.splice(colIndex, 1);
+    }
+  }
+
 
 }
